cafad94555194e7c699bdad0a3360a2ce3a2611c
Sayandip Saha <145214083+SayandipSaha666@users.noreply.github.com>
2025-08-26 21:05:36 +0000

    lect2 committed

---
+++ 01_basics/lect2.js
@@ -0,0 +1,14 @@
+// How to declarre variables and Ids
+const accountId = 14443
+let accountEmail = "xyz@gmail.com"
+var accountPass = 12345
+accountCity = "Siliguri"
+let accountState // Undefined datatype -> prints Undefined
+
+accountEmail = "ss@it.com"
+accountPass = 45631
+accountCity = "Jalpaiguri"
+
+console.log(accountEmail)
+
+console.table([accountId,accountEmail,accountPass,accountCity,accountState])
